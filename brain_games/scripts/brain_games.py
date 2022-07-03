e544c66b1907f4a4487c0adc27e3ecedfe7655ba
Алексей Рощектаев <roschek@yandex.ru>
2022-07-04 00:14:29 +0300

    first

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,10 @@
+def start():
+    print('poetry run python -m brain_games.scripts.brain_games\nWelcome to the Brain Games!')
+
+
+def main():
+    start()
+
+
+if __name__ == '__main__':
+    main()
